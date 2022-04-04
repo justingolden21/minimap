e9eadb7a8c88624036abb7288c458c77121ca4e0
Justin Golden <justingolden21@gmail.com>
2022-04-04 11:25:11 -0700

    update for sdsu

--- minimap.user.js
+++ minimap.user.js
@@ -15,12 +15,12 @@
 
 const _TamperRoot = this;
 (async function () {
-  // Updater
-  GM.xmlHttpRequest({
-    method: "GET",
-    url: `https://raw.githubusercontent.com/ApionXD/minimap/d/main/minimap.impl.user.js?t=${new Date().getTime()}`,
-    onload: function (res) {
-      new Function(res.responseText)(_TamperRoot);
-    },
-  });
+	// Updater
+	GM.xmlHttpRequest({
+		method: 'GET',
+		url: `https://raw.githubusercontent.com/justingolden21/minimap/d/main/minimap.impl.user.js?t=${new Date().getTime()}`,
+		onload: function (res) {
+			new Function(res.responseText)(_TamperRoot);
+		},
+	});
 })();
